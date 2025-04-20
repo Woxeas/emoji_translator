c081c1939808ccb2b6fbb5a43e67e54583bf87fd
Antonín Bujnovský <120708132+MrBujnowski@users.noreply.github.com>
2025-04-20 14:51:04 +0200

    Added sentry

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+import 'dart:async';
 import 'app.dart';
 import 'services/supabase_service.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await initSupabase();
-  runApp(const EmojiTranslatorApp());
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    await initSupabase();
+
+    const sentryDsn = String.fromEnvironment('SENTRY_DSN');
+
+    await SentryFlutter.init(
+      (options) {
+        options.dsn = sentryDsn;
+        options.tracesSampleRate = 1.0;
+        options.debug = true;
+      },
+    );
+
+    runApp(const EmojiTranslatorApp());
+  }, (error, stackTrace) async {
+    await Sentry.captureException(error, stackTrace: stackTrace);
+  });
 }
